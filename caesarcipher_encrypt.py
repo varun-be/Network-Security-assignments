1bf2596fad9047a01b9d8c64290a0b16663a3301
varun-be <58982839+varun-be@users.noreply.github.com>
2020-02-18 16:54:30 +0530

    Create caesarcipher_encrypt.py

---
+++ caesarcipher_encrypt.py
@@ -0,0 +1,18 @@
+import os
+L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
+I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+key = 3
+path = '/home/pc/test'
+
+fileList = os.listdir(path)
+for i in fileList:
+	f=open(os.path.join(path,i), "r")
+
+	plaintext = ""
+	ciphertext = f.read()
+	for c in ciphertext.upper():
+		if c.isalpha(): plaintext += I2L[ (L2I[c] - key)%26 ]
+		else: plaintext += c
+	f=open(os.path.join(path,i), "w+")
+	f.write(plaintext)
